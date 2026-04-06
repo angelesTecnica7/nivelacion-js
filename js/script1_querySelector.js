4c6a0fec08208a479b6018c4184a039b50ebe87f
angelesTecnica7 <angeles@tecnica7.edu.ar>
2026-04-06 16:34:01 -0300

    querySelector

---
+++ js/script1_querySelector.js
@@ -0,0 +1,18 @@
+console.log("hola mundo")
+titulo=document.querySelector('h1')
+console.log(titulo)
+titulo.innerHTML="<i>este es el titulo de mi página</i>"
+
+// document.querySelector('h1').innerHTML="TITULO"
+
+section=document.querySelector('#seccion')
+console.log(section)
+section.style.background='red'
+
+textos=document.querySelector('.texto')
+console.log(textos)
+textos.innerHTML="mi texto"
+
+
+
+
